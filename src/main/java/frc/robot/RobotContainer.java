e49caf095f3dcf3b3050a124cee3f020e1e8a0e5
SirPicklJohn <sirpickljohn@gmail.com>
2020-03-17 18:49:57 -0400

    super tiny adjustment1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,7 +20,6 @@ import frc.robot.commands.DriveForwardDistance;
 import frc.robot.commands.HalfDrive;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
